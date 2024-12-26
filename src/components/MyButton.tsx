fbd340d615a6fc2a3a909036a813ff39872eabe8
RomanGorenkov812 <roman.gorenkov@tobania.be>
2024-12-26 20:08:13 +0300

    temp

---
+++ src/components/MyButton.tsx
@@ -0,0 +1,14 @@
+export default function MyButton() {
+    const myButtonStyle: string = 'm-8 p-4 bg-amber-200 rounded-lg flex';
+
+    return (
+
+        <div className={myButtonStyle}>
+            <input/>
+
+            <button>
+                loool
+            </button>
+        </div>
+    )
+}
